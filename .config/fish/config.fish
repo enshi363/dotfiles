e58c3667119f2317a5aadfe96cac3ab1d025525d
sh <enshi363@qq.com>
2024-09-23 09:04:51 +0800

    aa

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,18 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+set -x PUB_HOSTED_URL "https://pub.flutter-io.cn"
+set -x FLUTTER_STORAGE_BASE_URL "https://storage.flutter-io.cn"
+
+
+set -U fish_greeting ""
+set -x TMUX_TMPDIR $HOME/.config/tmux/tmp
+
+
+set -x GO111MODULE on
+set -x CGO_ENABLED 0
+set -x GOPROXY https://goproxy.cn
+
+set -x FZF_DEFAULT_COMMAND 'rg --files'
+set -x FZF_DEFAULT_OPTS '-m --height 50% --border'
--- init.sh
+++ init.sh
@@ -9,10 +9,17 @@ mv ~/.config/fish ~/.config/fish.old
 mv ~/.config/kitty ~/.config/kitty.old
 mv ~/.config/nvim ~/.config/nvim.old
 mv ~/.config/tmux ~/.config/tmux.old
+mv ~/.config/wofi ~/.config/wofi.old
 
-ln -s  .config/alacritty ~/.config/alacritty
-ln -s  .config/fish ~/.config/fish
-ln -s  .config/kitty ~/.config/kitty
-ln -s  .config/nvim ~/.config/nvim
-ln -s  .config/tmux ~/.config/tmux
+CURRENTDIR=$(pwd)
+
+echo  $CURRENTDIR/.config/alacritty 
+
+ln -s  $CURRENTDIR/.config/alacritty ~/.config/alacritty
+ln -s  $CURRENTDIR/.config/fish ~/.config/fish
+ln -s  $CURRENTDIR/.config/kitty ~/.config/kitty
+ln -s  $CURRENTDIR/.config/nvim ~/.config/nvim
+ln -s  $CURRENTDIR/.config/tmux ~/.config/tmux
+ln -s  $CURRENTDIR/.config/wofi ~/.config/wofi
+
 
