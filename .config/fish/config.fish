aa64ab68b538ac4ed70480bdbf70c0a58be2fa5b
sh <enshi363@qq.com>
2025-04-01 18:22:06 +0800

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -19,7 +19,12 @@ set -x GOPROXY https://goproxy.cn
 set -x FZF_DEFAULT_COMMAND 'rg --files'
 set -x FZF_DEFAULT_OPTS '-m --height 50% --border'
 
+set -x  HOMEBREW_BREW_GIT_REMOTE "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
+set -x  HOMEBREW_CORE_GIT_REMOTE "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
+set -x  HOMEBREW_INSTALL_FROM_API 1 
+
 set -Ua fish_user_paths $HOME/go/bin/
+set -Ua fish_user_paths /opt/homebrew/bin 
 
 if test -f $HOME/.config/custom.fish
     source $HOME/.config/custom.fish
