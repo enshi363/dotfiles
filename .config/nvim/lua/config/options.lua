1ab9a82d6cf82624b92cc43b3cb77b5f62fe15a0
sh <enshi363@qq.com>
2025-10-08 20:08:08 +0800

    update

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,6 +3,9 @@
 -- Add any additional options here
 
 -- vim.g.snacks_animate = false
-vim.g.lazyvim_picker = "telescope"
+-- vim.g.lazyvim_picker = "telescope"
 vim.g.lazyvim_php_lsp = "intelephense"
 vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
+-- vim.g.lazyvim_cmp = "nvim-cmp"
+vim.g.lazyvim_cmp = "blink.cmp"
+-- vim.g.ai_cmp = false
---
+++ .config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,46 @@
+return {
+  "saghen/blink.cmp",
+  -- optional: provides snippets for the snippet source
+  dependencies = { "rafamadriz/friendly-snippets", "onsails/lspkind.nvim", "nvim-tree/nvim-web-devicons" },
+  opts = {
+    completion = {
+      trigger = {
+        -- show_in_snippet = false,
+      },
+      menu = {
+        draw = {
+          columns = {
+            { "label", "label_description", gap = 1 },
+            { "kind_icon" },
+          },
+          components = {
+            kind_icon = {
+              text = function(ctx)
+                local icon = ctx.kind_icon
+                if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                  local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+                  if dev_icon then
+                    icon = dev_icon
+                  end
+                else
+                  icon = require("lspkind").symbolic(ctx.kind, {
+                    mode = "symbol_text",
+                  })
+                end
+
+                return icon .. ctx.icon_gap
+              end,
+            },
+          },
+        },
+      },
+    },
+    keymap = {
+      ["<Tab>"] = {
+        "select_next",
+        "fallback",
+      },
+      ["<S-Tab>"] = { "select_prev", "fallback" },
+    },
+  },
+}
