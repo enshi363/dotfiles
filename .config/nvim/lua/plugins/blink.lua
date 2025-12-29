5cb10e01d23c9440fd4cf312c4661cc1b16d8d77
sh <shi.en@opg.cn>
2025-12-29 17:31:22 +0800

    tmux config update

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -2,6 +2,7 @@ return {
   "saghen/blink.cmp",
   -- optional: provides snippets for the snippet source
   dependencies = { "rafamadriz/friendly-snippets", "onsails/lspkind.nvim", "nvim-tree/nvim-web-devicons" },
+  version = "*",
   opts = {
     completion = {
       trigger = {
